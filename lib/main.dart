224ecbb498f8aa48b0a0248861befae84db12265
Jose Angel Nieda <jose.angel@tii.ae>
2021-11-29 20:21:22 +0000

    :sparkles: Add container example

--- lib/main.dart
+++ lib/main.dart
@@ -13,17 +13,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData(brightness: Brightness.dark),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -41,25 +34,21 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          bottom: const TabBar(
-            tabs: [
-              Tab(icon: Icon(Icons.flight)),
-              Tab(icon: Icon(Icons.directions_transit)),
-              Tab(icon: Icon(Icons.directions_car)),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Centered container example'),
+      ),
+      body: Center(
+        child: Container(
+          child: const Center(
+            child: Text(
+              'Hey there CESINF',
+              textAlign: TextAlign.center,
+            ),
           ),
-          title: const Text('Tabs Demo'),
-        ),
-        body: const TabBarView(
-          children: [
-            Icon(Icons.flight, size: 350),
-            Icon(Icons.directions_transit, size: 350),
-            Icon(Icons.directions_car, size: 350),
-          ],
+          color: Colors.blue,
+          height: 100,
+          width: 100,
         ),
       ),
     );
